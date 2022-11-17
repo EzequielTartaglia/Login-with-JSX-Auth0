53f88f88cace9631b552f65c0a2ecc335b4665d9
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2022-11-17 06:20:47 -0300

    Cleaning code

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,21 +1,21 @@
 //Estilos desde .css
-import './Styles/App.css';
-import'./Styles/NavegadorBar.css'
+import '../Styles/App.css';
+import'../Styles/NavegadorBar.css';
 //Paginas para enlazar (SPA)
-import Inicio from './Pages/Inicio';
-import TareasCompletas from './Pages/TareasCompletas';
-import TareasIncompletas from './Pages/TareasIncompletas';
+import Inicio from './Inicio';
+import TareasCompletas from './TareasCompletas';
+import TareasIncompletas from './TareasIncompletas';
 import { Route, Routes } from 'react-router-dom';
 
 //Componentes para funcion login
-import { Perfil } from './Componentes/Perfil';
-import NavegadorBar from './Componentes/NavegadorBar'
+import { Perfil } from '../Componentes/Perfil';
+import NavegadorBar from '../Componentes/NavegadorBar'
 
 function App() {
 
   
    //Efecto de carga
-/*   const {isLoading} = useAuth0()
+  /*   const {isLoading} = useAuth0()
   if(isLoading) return <h1>Cargando...</h1> */
 
   return (
